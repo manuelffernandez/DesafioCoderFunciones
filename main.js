037ca9ae88ec1a7fb16dbf6505e467eff3a3b65b
Manuel Fernandez <109448383+manuelffernandez@users.noreply.github.com>
2022-09-04 23:31:49 -0300

    Hotfix & change name
    Correct an error that shows the total price of the order inaccurate. This was fixed by changing the scope of the variable "total", placing it in the block of the "showOrder()" function.
    Also, change the name of the const "FRASE_CORREGIR" by "FRASE_MODIFICAR" to avoid misinterpretation.

--- main.js
+++ main.js
@@ -1,9 +1,7 @@
 const FRASE_BIENVENIDO = 'BIENVENIDO\n\nBienvenido al E-shop de café. Seleccione una opción.\n1.Ordenar pedido\n2.Salir';
 const FRASE_COMPRA = 'COMPRA\n\nQue producto desea:\n1. Café  $30\n2. Jugo  $25\n3. Medialuna  $15\n4. Sandwich  $35';
-const FRASE_CORRREGIR = 'CORREGIR\n\nQue desea hacer:\n1.Seguir comprando\n2.Ir al carrito';
+const FRASE_MODIFICAR = 'MODIFICAR\n\nQue desea hacer:\n1.Seguir comprando\n2.Ir al carrito';
 const FRASE_CARRITO = 'CARRITO\n\nRevise la consola para ver su pedido. Que desea hacer:\n1.Finalzar compra\n2.Volver';
-
-let total = 0;
 
 let cart = {
 	1: {
@@ -83,10 +81,13 @@ function getOrder() {
 	
 	incrementCart(elegido, cantidad);
 
-	menu(correctUserChoice(getUserChoice(FRASE_CORRREGIR, 2), 2, 1));
+	menu(correctUserChoice(getUserChoice(FRASE_MODIFICAR, 2), 2, 1));
 }
 
 function showOrder() {
+	let total = 0;
+
+	console.log('-'.repeat(40))
 	console.log('Tu pedido es:');
 	for(const [key, obj] of Object.entries(cart)) {
 		if(obj.quantity !== 0) {
@@ -109,8 +110,8 @@ function menu(section) {
 			getOrder();
 			break;
 		case 2:
-		//CORREGIR
-			menu(correctUserChoice(getUserChoice(FRASE_CORRREGIR, 2), 2, 1));
+		//MODIFICAR
+			menu(correctUserChoice(getUserChoice(FRASE_MODIFICAR, 2), 2, 1));
 			break;
 		case 3:
 		//CARRITO
