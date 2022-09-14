f1fb353e897e9559606d1c6a50c63b2b48407d74
Manuel Fernandez <manufer6503@gmail.com>
2022-09-14 03:28:29 -0300

    FINISH THE LOGIC
    Made changes explain in the next commit

--- main.js
+++ main.js
@@ -4,12 +4,15 @@
 
 
 
-
+let executeProgram = true;
 
 const FRASE_BIENVENIDO = 'BIENVENIDO\nBienvenido al E-shop de café. Seleccione una opción:\n\n';
-const FRASE_COMPRA = 'COMPRA\nQue producto desea:\n\n';
-const FRASE_MODIFICAR = 'MODIFICAR\n\nQue desea hacer:\n1.Seguir comprando\n2.Ir al carrito';
+const FRASE_COMPRA_PRODUCTO = 'COMPRA\nQue producto desea:\n\n';
+const FRASE_COMPRA_CANTIDAD = 'COMPRA\nElija la cantidad de ';
+const FRASE_MODIFICAR_PRODUCTO = 'MODIFICAR\nSeleccione el producto del cual desea modificar la cantidad:\n\n';
+const FRASE_MODIFICAR_CANTIDAD = 'MODIFICAR\nElija la cantidad que desea modificar de: ';
 const FRASE_CARRITO = 'CARRITO\n\nRevise la consola para ver su pedido. Que desea hacer:\n1.Finalzar compra\n2.Volver';
+const FRASE_SOLAPAMIENTO_PEDIDO = 'Usted ya tiene un pedido en el carrito.\n Si crea uno nuevo el anterior pedido se borrará. Está seguro?\n\n'
 
 let store = [
 	{
@@ -47,7 +50,7 @@ let mainMenu =  [
 	{
 		name: 'Modificar compra',
 		input: '2',
-		func: 0
+		func: modifyOrder
 	},
 	{
 		name: 'Ver carrito',
@@ -55,9 +58,14 @@ let mainMenu =  [
 		func: showOrder	
 	},
 	{
+		name: 'Ver store',
+		input: '4',
+		func: showStoreStock	
+	},
+	{
 		name: 'Finalizar compra',
 		input: '0',
-		func: 0
+		func: endProgram
 	}
 ];
 
@@ -71,23 +79,40 @@ let productsMenu = store.map(element => {
 	input: '0'
 });
 
+let eraseCartMenu = [
+	{
+		name: 'Hacer un pedido nuevo',
+		input: '1'
+	},
+	{
+		name: 'Volver atrás',
+		input: '2'
+	}
+];
+
 let cart = [];
 
+
+function modulo(a) {
+	if(a < 0) {
+		return -a;
+	}
+	return a
+}
 
 function alertUser() {
 	alert('El valor ingresado no es correcto. Porfavor coloque el número correspondiente a la opción que desea.');
 }
 
 //Pide que ingrese una opcion y devuelve lo elegido. Se repite si la opcion no es valida.
-function getUserChoice(frase, array) {
+function getUserChoice(frase, menuArray) {
 	let desicion;
 
 	while(true) {
-		desicion = parseFloat(prompt(frase + showOptions(array)));
+		desicion = prompt(frase + showOptionsOfMenu(menuArray));
 
-		for (let i = 0; i <= array.length; i++) {
-			if (desicion === i) {
-				desicion = desicion.toString();
+		for (let i = 0; i < menuArray.length; i++) {
+			if (desicion == menuArray[i]['input']) {
 				return desicion;
 			}
 		}
@@ -96,12 +121,16 @@ function getUserChoice(frase, array) {
 }
 
 //Verifica que se escriba un numero en el prompt de getOrder() y retorna la cantidad
-function getOrderQuantity() {
-
+function getProductQuantity(frase, productFromStore, canReturnNegativeAmount = false) {
 	while (true) {
-		let amount = parseInt(prompt('COMPRA\n\nElija la cantidad:'));
+		let amount = parseInt(prompt(frase + productFromStore.name));
+		let isAmountNeg;
 
-		if (isNaN(amount) || amount < 0) {
+		if(canReturnNegativeAmount) {
+			isAmountNeg = false;
+		} else isAmountNeg = amount < 0;
+
+		if (isNaN(amount) || isAmountNeg) {
 			alertUser();
 			continue;
 		} else {
@@ -110,83 +139,131 @@ function getOrderQuantity() {
 	}
 }
 
-function findProductInCart(productName) {
-	return cart.find( item => item.name === productName);
+function findProductIn(arrayWhereSearch, productInputNumber) {
+	return arrayWhereSearch.find( item => item.input === productInputNumber);
 }
 
-function findProductInStore(input) {
-	return store.find(element => element.input == input);
-}
-
-function update(productFromStore, quantity) { 
-	let cartObject = findProductInCart(productFromStore);
-
-	productFromStore.stock -= quantity;
+function updateCartAndStore(productObjectToSearchInCart, quantity) { //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	let cartObject = findProductIn(cart, productObjectToSearchInCart.input);
+	let storeObject = store.find(element => element.input == productObjectToSearchInCart.input);
 
 	if(cartObject === undefined) {
 		cart = [...cart, {
-			name: productFromStore.name,
-			qty: quantity
+			name: productObjectToSearchInCart.name,
+			stock: quantity,
+			input: productObjectToSearchInCart.input
 		}];
+		storeObject.stock -= quantity;
 	} else {
-		cartObject.qty += quantity;
+		cartObject.stock += quantity;
+		storeObject.stock.stock -= quantity;
+	}
+
+	for(let item of cart) {
+		if (item.stock === 0) {
+			cart.splice(cart.indexOf(item), 1)
+		}
 	}
 }
 
-function checkStock(productFromStore, stockToDecrement) { 
-	if(productFromStore.stock < stockToDecrement) {
-		alert('No hay stock disponible.');
+function checkStock(productToCheck, stockToDecrement) { 
+	if(productToCheck.stock < stockToDecrement) {
+		alert(`No hay stock disponible. Puede agregar hasta ${productToCheck.stock} unidad/es más.`);
 		return false;
 	}
 	return true;
 }
 
-function showOptions(array) {
-	let titlesArray = array.map(elemento => elemento.input + '. ' + elemento.name);
+function showOptionsOfMenu(menuArray) {
+	let titlesArray = menuArray.map(elemento => elemento.input + '. ' + elemento.name);
 	let titles = titlesArray.join('\n');
 
 	return titles;
 }
 
-//Pide producto y cantidad y muestra por consola
-function getOrder() {
+function defineProductAndAmount(arrayWhereSearchProductObject, fraseChoice, fraseQuantity, arrayToShowAvailableProducts, canReceiveNegativeAmount) {
 	while(true) {
-		let chosenOption = getUserChoice(FRASE_COMPRA, productsMenu);
+		let chosenOption = getUserChoice(fraseChoice, arrayToShowAvailableProducts);
 
 		if(chosenOption == '0') {
 			return
 		}
 
-		let quantity = getOrderQuantity();
-		let product = findProductInStore(chosenOption);
+		let productObject = findProductIn(arrayWhereSearchProductObject, chosenOption); 
+		console.log(productObject);
+		let quantity = getProductQuantity(fraseQuantity, productObject, canReceiveNegativeAmount);
 
-		if(checkStock(product, quantity)) {
-			update(product, quantity);
+		if(checkStock(productObject, modulo(quantity))) {
+			updateCartAndStore(productObject, quantity);
 		}
 	}
 }
 
-function showOrder() {
-	alert('Chequee la consola porfavor');
+function getOrder() {
+	if(cart.length !== 0) {
+		let desicion = getUserChoice(FRASE_SOLAPAMIENTO_PEDIDO, eraseCartMenu);
+
+		if(desicion == '2') {
+			return
+		}
+
+		for(let i = 0; i < cart.length; i++) {
+			updateCartAndStore(cart[i], -cart[i]['stock'])
+		}
+	}
+	defineProductAndAmount(store, FRASE_COMPRA_PRODUCTO, FRASE_COMPRA_CANTIDAD, store.concat({
+		name: 'Volver al menú principal',
+		input: '0'
+	}), false);
+}
+
+function modifyOrder() {
+	if(cart.length == 0) {
+		alert('Usted no tiene ninguna orden para modificar.');
+		return
+	}
+	// debugger
+	defineProductAndAmount(cart, FRASE_MODIFICAR_PRODUCTO, FRASE_MODIFICAR_CANTIDAD, cart.concat({
+		name: 'Volver al menú principal',
+		input: '0'
+	}), true);
+}
+
+function showStoreStock() {
+	console.log('Lo que queda en la tienda:');
+	console.table(store);
+}
+
+function showCart() {
 	let total = 0;
 
-	console.log('-'.repeat(70));
-	console.log('Lo que queda en la tienda');
-	console.table(store);
 	console.log('Tu pedido es:');
-
 	cart.forEach(product => {
 		let productPrice = store.find(element => element.name === product.name)['price'];
-		let subtotal = product.qty * productPrice;
+		let subtotal = product.stock * productPrice;
 		total += subtotal;
-		console.log(`${product.qty} ${product.name} subtotal: $${subtotal}`);
+		console.log(`${product.stock} ${product.name} subtotal: $${subtotal}`);
 	});
 	console.log('Total: $' + total);
+}
+
+function showOrder() {
+	alert('Chequee la consola porfavor');
+	console.log('-'.repeat(70));
+
+	showCart();
+
 	console.log('-'.repeat(70));
 }
 
+function endProgram() {
+	alert(`Gracias por su compra!`);
+	showCart();
+	executeProgram = false;
+}
+
 function initMenu(menu) {
-	while(true) {
+	while(executeProgram) {
 		let desicion = getUserChoice(FRASE_BIENVENIDO, mainMenu);
 
 		for (let i = 0; i < menu.length; i++) {
